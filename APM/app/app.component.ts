2920c4d2ecdb5cea80e22e9719f1066fc98f0c03
RadSt <desan1986@gmail.com>
2016-06-03 22:08:08 +0300

    try ngif and ngfor

--- APM/app/app.component.ts
+++ APM/app/app.component.ts
@@ -1,12 +1,19 @@
 import {Component} from 'angular2/core';
+
+import { ProductListComponent } from './products/product-list.component'
+
 // селектор исп-ся для вызова DOM в index.html
 // Компонент декоратор исп-ся для хранения html разметки
+// `разметка` когда нужен кусок html 
+// "разметка" для строки html
+// ИСпользование компонента продукт лист в ап компоненте 
 @Component({
    selector: 'pm-app',
    template: `<div><h1>{{pageTitle}}</h1>
-   <div>My First Component</div>
+       <pm-products></pm-products>
    </div>
-   `          
+   `,
+   directives: [ProductListComponent]         
 })
 // Переменная со значением
 export class AppComponent {
